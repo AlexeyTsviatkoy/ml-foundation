43f1341efa085e2940c44fd395d915c75742b378
AlexeyTsviatkou <lexatsv.work@gmail.com>
2026-03-10 10:11:58 +0300

    pandas task 2

--- data/pandas_task2.py
+++ data/pandas_task2.py
@@ -2,4 +2,11 @@ import pandas as pd
 
 df = pd.read_csv('/home/alexey/Документы/Projects/ml-foundation/datasets/people.csv')
 
-print(df["age"].mean(), df["salary"].mean(),df["city"][df["city"].idxmax()])
+m_city = df["city"].value_counts().index[0]
+mean_salary = df["salary"].mean()
+
+print(df["age"].mean(),
+      mean_salary,
+      m_city,
+      df.groupby("city")["salary"].mean(),"\n",
+      df.loc[df["salary"] >= mean_salary,"name"])
